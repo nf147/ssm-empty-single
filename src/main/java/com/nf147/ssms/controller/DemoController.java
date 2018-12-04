795552a271c433830b1e14f9a6c2b19fc63259d7
niu <teacher@g.com>
2018-12-04 15:29:26 +0800

    restful 编码

--- src/main/java/com/nf147/ssms/controller/DemoController.java
+++ src/main/java/com/nf147/ssms/controller/DemoController.java
@@ -3,10 +3,7 @@ package com.nf147.ssms.controller;
 
 import com.nf147.ssms.demo_formatter.Person;
 import com.nf147.ssms.demo_formatter.PersonFromId;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/ddd")
 @RestController
@@ -15,5 +12,10 @@ public class DemoController {
     public Person home (@RequestParam(value = "id", required = false)
                             @PersonFromId("en") Person person) {
         return person;
+    }
+
+    @GetMapping("/code")
+    public String code(){
+        return "msg:这是测试";
     }
 }
