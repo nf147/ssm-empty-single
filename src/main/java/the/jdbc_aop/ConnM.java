fcfcea9e905689b09a7d7006587acf31727263f5
niu <teacher@g.com>
2018-12-19 11:36:29 +0800

    Spring

---
+++ src/main/java/the/jdbc_aop/ConnM.java
@@ -0,0 +1,11 @@
+package the.jdbc_aop;
+
+import java.sql.Connection;
+
+public class ConnM {
+    public static ThreadLocal<Connection> threadLocal = null;
+
+    public static Connection getConn() {
+        return threadLocal.get();
+    }
+}
