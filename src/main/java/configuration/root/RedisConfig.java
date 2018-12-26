fabd19d0454fea20ed89f0d754bc4e9fc69ba1ce
niu <teacher@g.com>
2018-12-26 10:50:57 +0800

    dsfsdfsdfsd

--- src/main/java/configuration/root/RedisConfig.java
+++ src/main/java/configuration/root/RedisConfig.java
@@ -1,7 +1,21 @@
 package configuration.root;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
 public class RedisConfig {
+    @Bean
+    RedisConnectionFactory redisFactory () {
+        return new JedisConnectionFactory();
+    }
+
+    @Bean
+    RedisTemplate redisTemplate () {
+        return new StringRedisTemplate(redisFactory());
+    }
 }
